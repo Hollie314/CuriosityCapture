d2d1106e1c04edc8f4566983df7363541f4f949c
MontyLois <lois.monty@outlook.fr>
2025-03-25 16:26:02 +0100

    spline animate && cursor && mask
    whaaat have i dooonee
    proto for wander patern && mask

---
+++ Assets/Scripts/Curiosity.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Curiosity : MonoBehaviour
+{
+
+    private float speed;
+
+    private string  name;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+}
