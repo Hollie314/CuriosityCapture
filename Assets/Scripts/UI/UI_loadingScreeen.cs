dc6f3ce521693b3f28db1f54d7ec62c7ca2acdbd
MontyLois <lois.monty@outlook.fr>
2025-03-28 17:36:12 +0100

    Main menue scene

---
+++ Assets/Scripts/UI/UI_loadingScreeen.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_loadingScreeen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main_Scene");
+    }
+}
